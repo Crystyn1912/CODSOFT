ebc14a1540ce269a372a2e28adee86173c5aa786
Dhriti <dhritimittal.10a@gmail.com>
2023-09-22 13:02:39 +0530

    Create number-guessing-game.cpp

--- Number guessing game/number-guessing-game.cpp
+++ Number guessing game/number-guessing-game.cpp
@@ -1,3 +1,6 @@
+/* Create a program that generates a random number and asks the user to guess it. Provide feedback on whether
+the guess is too high or too low until the user guesses the correct number.*/
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -11,16 +14,23 @@ int main()
 
     srand(time(nullptr));
     short num = (rand() % (max_value - min_value + 1)) + min_value;
-
+    
     short guess;
-    cout << "Guess a number between 1 and 100 : ";
-    cin >> guess;
 
-    if(guess < num)
-      { cout << "Your guess is too low. The number was : " << num; }
-    else if(guess > num)
-      { cout << "Your guess is too high. The number was : " << num; }
-    else
-      { cout << "You guessed right."; }   
+    do
+    {
+      cout << "Guess a number between 1 and 100 : ";
+      cin >> guess;
+      
+      if(guess < num)
+        { cout << "Your guess is too low." << endl; }
+      else if(guess > num)
+        { cout << "Your guess is too high." << endl; }
+      else if(guess == num)
+        { cout << "You guessed right." << endl; }
+    }
+    
+    while ( guess != num );
+    
     return 0;  
 }
