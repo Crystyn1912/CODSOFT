b5fab56498e90e23b816f126f6a25f5c96042f24
Dhriti <dhritimittal.10a@gmail.com>
2023-10-04 21:59:26 +0530

    Updated

---
+++ Word counter/word-counter.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() 
+{
+    string filename;
+    cout << "Enter the name of the file you want to count the words of : ";
+    cin >> filename;
+
+    ifstream inputFile(filename);
+    if (inputFile.is_open()) 
+    {
+      string line;
+      int wordCount = 0;
+
+      while (getline(inputFile, line)) 
+      {
+        stringstream ss(line);
+        string word;
+
+        while (ss >> word) 
+        {
+          wordCount++;
+        }
+      }
+
+      inputFile.close();
+      cout << "Number of words in the said file: " << wordCount << endl;
+    } 
+    else 
+    {
+    cout << "Failed to open the file." << endl;
+    }
+    return 0;
+}
