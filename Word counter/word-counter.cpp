08d9ca296d64894eabafec56010e1404687fb186
Dhriti <dhritimittal.10a@gmail.com>
2023-10-14 23:59:25 +0530

    Updated

--- Word counter/word-counter.cpp
+++ Word counter/word-counter.cpp
@@ -1,3 +1,6 @@
+/* Develop a program that counts the number of words in a given text file. Prompt the user to enter the file 
+name and display the total word count. */
+
 #include <iostream>
 #include <fstream>
 #include <string>
